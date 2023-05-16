0229b6accaa17bf75e519f1330299bbd3616f20b
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-05-16 15:32:24 +0530

    changrd

---
+++ Question24/src/main/java/in/ineuron/Question24Application.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Question24Application.class, args);
+	}
+
+}
