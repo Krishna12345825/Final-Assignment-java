007a134dda084cc919e55bc169381445a29f6f00
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-05-20 08:10:34 +0530

    Question 29

---
+++ Question29/src/main/java/in/ineuron/Question29Application.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Question29Application.class, args);
+	}
+
+}
